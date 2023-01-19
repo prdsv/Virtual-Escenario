061cdd4b5c6f00ecef4e1d2f5553846c563387bc
prdsv <121121600+prdsv@users.noreply.github.com>
2023-01-19 19:52:27 +0100

    Add files via upload

---
+++ permisos.sh
@@ -0,0 +1,12 @@
+chmod 777 cpechart-0.1.0.tgz 
+chmod 777 deploy.sh
+chmod 777 helm
+chmod 777 img
+chmod 777 index-yaml
+chmod 777 index.yaml
+chmod 777 osm_renes1.sh 
+chmod 777 osm_renes2.sh 
+chmod 777 osm_renes_start.sh 
+chmod 777 pck
+chmod 777 renes_start.sh
+chmod 777 vnx
