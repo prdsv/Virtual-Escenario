105609080b7c22f8d73366b5bbc87296e0d85ef1
prdsv <121121600+prdsv@users.noreply.github.com>
2023-01-22 14:57:55 +0100

    Update permisos.sh

--- permisos.sh
+++ permisos.sh
@@ -1,3 +1,10 @@
+chmod 777 deployk8.sh
+chmod 777 arpwatch.sh
+chmod 777 deployk8.sh
+chmod 777 destroyk8.sh
+
+cd practica/rdsv-final
+
 chmod 777 cpechart-0.1.0.tgz 
 chmod 777 deploy.sh
 chmod 777 helm
