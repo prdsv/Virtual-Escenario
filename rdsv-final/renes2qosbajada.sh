5c0ed108e0f4bc317dc928305238c3036b8332e6
prdsv <121121600+prdsv@users.noreply.github.com>
2023-01-29 21:23:43 +0100

    Update renes2qosbajada.sh

--- rdsv-final/renes2qosbajada.sh
+++ rdsv-final/renes2qosbajada.sh
@@ -1,24 +1,22 @@
 set -u # to verify variables are defined
 : $KUBECTL
 : $OSMNS
-: $H11
-: $H12
 : $H21
 : $H22
 
-export VACC="deploy/$OSMACC"
-export VCPE="deploy/$OSMCPE"
 
 
-ACC_EXEC="$KUBECTL exec -n $OSMNS $VACC --"
-CPE_EXEC="$KUBECTL exec -n $OSMNS $VCPE --"
+export VACC1="deploy/$OSMACC"
+export VCPE1="deploy/$OSMCPE"
 
+ACC_EXEC="$KUBECTL exec -n $OSMNS $VACC1 --"
+CPE_EXEC="$KUBECTL exec -n $OSMNS $VCPE1 --"
 
-$ACC_EXEC curl -X PUT -d '"tcp:127.0.0.1:6632"' http://localhost:8080/v1.0/conf/switches/0000000000000001/ovsdb_addr
+$ACC_EXEC curl -X PUT -d '"tcp:10.255.0.4:6632"' http://localhost:8080/v1.0/conf/switches/0000000000000001/ovsdb_addr
 sleep 10
-# Configuramos 12 Mbps de bajada de la red residencial y la bajada en las colas (hX1(Cola 1) y hX2(Cola 0))
 
-$ACC_EXEC curl -X POST -d '{"port_name": "vxlanacc", "type": "linux-htb", "max_rate": "12000000", "queues": [{"max_rate": "4000000"}, {"min_rate": "8000000"}]}' http://localhost:8080/qos/queue/0000000000000001
+$ACC_EXEC curl -X POST -d '{"port_name": "vxlanint1", "type": "linux-htb", "max_rate": "6000000", "queues": [{"max_rate": "2000000"}, {"min_rate": "4000000"}]}' http://localhost:8080/qos/queue/0000000000000001
 sleep 10
-$ACC_EXEC curl -X POST -d '{"match": {"nw_dst": "'$H22'"}, "actions":{"queue": "0"}}' http://localhost:8080/qos/rules/0000000000000001
-$ACC_EXEC curl -X POST -d '{"match": {"nw_dst": "'$H21'"}, "actions":{"queue": "1"}}' http://localhost:8080/qos/rules/0000000000000001
+
+$ACC_EXEC curl -X POST -d '{"match": {"nw_src": "'$H22'"}, "actions":{"queue": "0"}}' http://localhost:8080/qos/rules/0000000000000001
+$ACC_EXEC curl -X POST -d '{"match": {"nw_src": "'$H21'"}, "actions":{"queue": "1"}}' http://localhost:8080/qos/rules/0000000000000001
