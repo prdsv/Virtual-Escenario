dad051545c9c9184bbfc5ca40bcfb00e29372252
prdsv <121121600+prdsv@users.noreply.github.com>
2023-01-29 20:10:43 +0100

    Delete permisos.sh

--- rdsv-final/permisos.sh
+++
@@ -1,6 +0,0 @@
-
-
-sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x dhclient-h11
-sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x dhclient-h12
-sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x dhclient-h21
-sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x dhclient-h22
