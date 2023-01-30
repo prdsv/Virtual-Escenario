40d436631661fdd3ff8e48c7ec2c1f5a0c043a07
prdsv <121121600+prdsv@users.noreply.github.com>
2023-01-30 21:51:10 +0100

    Update renes_start.sh

--- rdsv-final/renes_start.sh
+++ rdsv-final/renes_start.sh
@@ -59,11 +59,14 @@ echo "## 3. En VNF:access agregar un bridge y configurar IPs y rutas"
 
 $ACC_EXEC ovs-vsctl add-br brint
 $ACC_EXEC ovs-vsctl set bridge brint protocols=OpenFlow10,OpenFlow12,OpenFlow13
-#$ACC_EXEC ovs-vsctl set-fail-mode brint secure
 $ACC_EXEC ovs-vsctl set bridge brint other-config:datapath-id=0000000000000001
-$ACC_EXEC ovs-vsctl set-controller brint tcp:127.0.0.1:6633
-$ACC_EXEC ovs-vsctl set-manager ptcp:6632
 $ACC_EXEC ifconfig net1 $VNFTUNIP/24
+
+
+#$ACC_EXEC ovs-vsctl set-fail-mode brint secure
+
+
+
 
 $ACC_EXEC ip link add vxlanacc type vxlan id 0 remote $HOMETUNIP dstport 4789 dev net1
 # En la siguiente línea se ha corregido el dispositivo, que debe ser eth0
@@ -73,9 +76,12 @@ $ACC_EXEC ovs-vsctl add-port brint vxlanint
 $ACC_EXEC ifconfig vxlanacc up
 $ACC_EXEC ifconfig vxlanint up
 
-$ACC_EXEC ip route add $IPCPE/32 via $K8SGW
+$ACC_EXEC ovs-vsctl set-controller brint tcp:127.0.0.1:6633
+$ACC_EXEC ovs-vsctl set-manager ptcp:6632
+
 $ACC_EXEC ryu-manager ryu.app.rest_qos ryu.app.rest_conf_switch ./qos_simple_switch_13.py &
 
+$ACC_EXEC ip route add $IPCPE/32 via $K8SGW
 
 ## 4. En VNF:cpe agregar un bridge y configurar IPs y rutas
 echo "## 4. En VNF:cpe agregar un bridge y configurar IPs y rutas"
