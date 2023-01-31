6dfbd232ae2a699f5ca40aab53b2b3f7f7871ea7
prdsv <121121600+prdsv@users.noreply.github.com>
2023-01-31 22:16:20 +0100

    Update renes1qosdown.sh

--- rdsv-final/renes1qosdown.sh
+++ rdsv-final/renes1qosdown.sh
@@ -10,8 +10,9 @@ $ACC_EXEC curl -X PUT -d '"tcp:127.0.0.1:6632"' http://127.0.0.1:8080/v1.0/conf/
 sleep 10
 # Configuramos 12 Mbps de bajada de la red residencial y la bajada en las colas (hX1(Cola 1) y hX2(Cola 0))
 
-$ACC_EXEC curl -X POST -d '{"port_name": "vxlanacc", "type": "linux-htb", "max_rate": "12000000", "queues": [{"min_rate": "8000000"}, {"max_rate": "4000000"}]}' http://127.0.0.1:8080/qos/queue/0000000000000001
+$ACC_EXEC curl -X POST -d '{"port_name": "vxlanacc", "type": "linux-htb", "max_rate": "12000000", "queues": [{"max_rate": "4000000"}, {"min_rate": "8000000"}]}' http://127.0.0.1:8080/qos/queue/0000000000000001
 sleep 10
-$ACC_EXEC curl -X POST -d '{"match": {"dl_src": "02:fd:00:04:00:01", "dl_type": "IPv4"}, "actions":{"queue": "0"}}' http://127.0.0.1:8080/qos/rules/0000000000000001
-$ACC_EXEC curl -X POST -d '{"match": {"dl_src": "02:fd:00:04:01:01", "dl_type": "IPv4"}, "actions":{"queue": "1"}}' http://127.0.0.1:8080/qos/rules/0000000000000001
+$ACC_EXEC curl -X POST -d '{"match": {"dl_src": "02:fd:00:04:01:01", "dl_type": "IPv4"}, "actions":{"queue": "0"}}' http://127.0.0.1:8080/qos/rules/0000000000000001
+$ACC_EXEC curl -X POST -d '{"match": {"dl_src": "02:fd:00:04:00:01", "dl_type": "IPv4"}, "actions":{"queue": "1"}}' http://127.0.0.1:8080/qos/rules/0000000000000001
+
 
