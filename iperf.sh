9fc8fd773f7b9fa45822dd8d0d909540cd3e0aef
prdsv <121121600+prdsv@users.noreply.github.com>
2023-01-31 15:52:05 +0100

    Create iperf.sh

---
+++ iperf.sh
@@ -0,0 +1,5 @@
+#Se instala iperf3 en cada uno de los host de las redes residenciales
+sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x iperf3_h11
+sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x iperf3_h12
+sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x iperf3_h21
+sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x iperf3_h22
